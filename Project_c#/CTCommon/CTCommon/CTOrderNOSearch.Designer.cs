4544532bd2e47803ce30541a36c48c7b0b18c374
SmilePeco <63031036+SmilePeco@users.noreply.github.com>
2020-06-03 19:54:08 +0900

    2020/06/03 最新版

---
+++ Project_c#/CTCommon/CTCommon/CTOrderNOSearch.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace CTCommon
+{
+    partial class CTOrderNOSearch
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.GroupBox1 = new System.Windows.Forms.GroupBox();
+            this.dtpOrderDateTo = new System.Windows.Forms.DateTimePicker();
+            this.Label3 = new System.Windows.Forms.Label();
+            this.dtpOrderDateFrom = new System.Windows.Forms.DateTimePicker();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtOrderMSNo = new System.Windows.Forms.TextBox();
+            this.Label1 = new System.Windows.Forms.Label();
+            this.GroupBox3 = new System.Windows.Forms.GroupBox();
+            this.btnEnd = new System.Windows.Forms.Button();
+            this.GroupBox2 = new System.Windows.Forms.GroupBox();
+            this.DataGridView1 = new System.Windows.Forms.DataGridView();
+            this.GroupBox1.SuspendLayout();
+            this.GroupBox3.SuspendLayout();
+            this.GroupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.DataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // GroupBox1
+            // 
+            this.GroupBox1.Controls.Add(this.dtpOrderDateTo);
+            this.GroupBox1.Controls.Add(this.Label3);
+            this.GroupBox1.Controls.Add(this.dtpOrderDateFrom);
+            this.GroupBox1.Controls.Add(this.label4);
+            this.GroupBox1.Controls.Add(this.txtOrderMSNo);
+            this.GroupBox1.Controls.Add(this.Label1);
+            this.GroupBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.GroupBox1.Location = new System.Drawing.Point(0, 0);
+            this.GroupBox1.Name = "GroupBox1";
+            this.GroupBox1.Size = new System.Drawing.Size(560, 101);
+            this.GroupBox1.TabIndex = 10;
+            this.GroupBox1.TabStop = false;
+            // 
+            // dtpOrderDateTo
+            // 
+            this.dtpOrderDateTo.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpOrderDateTo.Location = new System.Drawing.Point(253, 53);
+            this.dtpOrderDateTo.Name = "dtpOrderDateTo";
+            this.dtpOrderDateTo.Size = new System.Drawing.Size(130, 19);
+            this.dtpOrderDateTo.TabIndex = 26;
+            // 
+            // Label3
+            // 
+            this.Label3.AutoSize = true;
+            this.Label3.Font = new System.Drawing.Font("MS UI Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.Label3.Location = new System.Drawing.Point(225, 55);
+            this.Label3.Name = "Label3";
+            this.Label3.Size = new System.Drawing.Size(22, 15);
+            this.Label3.TabIndex = 25;
+            this.Label3.Text = "～";
+            // 
+            // dtpOrderDateFrom
+            // 
+            this.dtpOrderDateFrom.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpOrderDateFrom.Location = new System.Drawing.Point(92, 53);
+            this.dtpOrderDateFrom.Name = "dtpOrderDateFrom";
+            this.dtpOrderDateFrom.Size = new System.Drawing.Size(130, 19);
+            this.dtpOrderDateFrom.TabIndex = 24;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 58);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 12);
+            this.label4.TabIndex = 23;
+            this.label4.Text = "受注日";
+            // 
+            // txtOrderMSNo
+            // 
+            this.txtOrderMSNo.Location = new System.Drawing.Point(92, 20);
+            this.txtOrderMSNo.Name = "txtOrderMSNo";
+            this.txtOrderMSNo.Size = new System.Drawing.Size(70, 19);
+            this.txtOrderMSNo.TabIndex = 5;
+            // 
+            // Label1
+            // 
+            this.Label1.AutoSize = true;
+            this.Label1.Location = new System.Drawing.Point(12, 24);
+            this.Label1.Name = "Label1";
+            this.Label1.Size = new System.Drawing.Size(57, 12);
+            this.Label1.TabIndex = 4;
+            this.Label1.Text = "受注先NO";
+            // 
+            // GroupBox3
+            // 
+            this.GroupBox3.Controls.Add(this.btnEnd);
+            this.GroupBox3.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.GroupBox3.Location = new System.Drawing.Point(0, 377);
+            this.GroupBox3.Name = "GroupBox3";
+            this.GroupBox3.Size = new System.Drawing.Size(560, 49);
+            this.GroupBox3.TabIndex = 11;
+            this.GroupBox3.TabStop = false;
+            // 
+            // btnEnd
+            // 
+            this.btnEnd.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnEnd.Location = new System.Drawing.Point(458, 17);
+            this.btnEnd.Name = "btnEnd";
+            this.btnEnd.Size = new System.Drawing.Size(75, 23);
+            this.btnEnd.TabIndex = 3;
+            this.btnEnd.Text = "F1:終了";
+            this.btnEnd.UseVisualStyleBackColor = true;
+            this.btnEnd.Click += new System.EventHandler(this.button_Click);
+            // 
+            // GroupBox2
+            // 
+            this.GroupBox2.Controls.Add(this.DataGridView1);
+            this.GroupBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.GroupBox2.Location = new System.Drawing.Point(0, 101);
+            this.GroupBox2.Name = "GroupBox2";
+            this.GroupBox2.Size = new System.Drawing.Size(560, 276);
+            this.GroupBox2.TabIndex = 13;
+            this.GroupBox2.TabStop = false;
+            // 
+            // DataGridView1
+            // 
+            this.DataGridView1.AllowUserToAddRows = false;
+            this.DataGridView1.AllowUserToDeleteRows = false;
+            this.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.DataGridView1.Location = new System.Drawing.Point(3, 15);
+            this.DataGridView1.Name = "DataGridView1";
+            this.DataGridView1.ReadOnly = true;
+            this.DataGridView1.RowTemplate.Height = 21;
+            this.DataGridView1.Size = new System.Drawing.Size(554, 258);
+            this.DataGridView1.TabIndex = 0;
+            this.DataGridView1.CellContentDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DataGridView1_CellContentDoubleClick);
+            // 
+            // CTOrderNOSearch
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(560, 426);
+            this.Controls.Add(this.GroupBox2);
+            this.Controls.Add(this.GroupBox1);
+            this.Controls.Add(this.GroupBox3);
+            this.KeyPreview = true;
+            this.Name = "CTOrderNOSearch";
+            this.Text = "受注NO検索";
+            this.Load += new System.EventHandler(this.CTOrderNOSearch_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.CTOrderNOSearch_KeyDown);
+            this.GroupBox1.ResumeLayout(false);
+            this.GroupBox1.PerformLayout();
+            this.GroupBox3.ResumeLayout(false);
+            this.GroupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.DataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        internal System.Windows.Forms.GroupBox GroupBox1;
+        internal System.Windows.Forms.TextBox txtOrderMSNo;
+        internal System.Windows.Forms.Label Label1;
+        internal System.Windows.Forms.GroupBox GroupBox3;
+        internal System.Windows.Forms.Button btnEnd;
+        internal System.Windows.Forms.GroupBox GroupBox2;
+        public System.Windows.Forms.DataGridView DataGridView1;
+        internal System.Windows.Forms.DateTimePicker dtpOrderDateTo;
+        internal System.Windows.Forms.Label Label3;
+        internal System.Windows.Forms.DateTimePicker dtpOrderDateFrom;
+        internal System.Windows.Forms.Label label4;
+    }
+}
---
+++ Project_c#/CTCommon/CTCommon/CTOrderNOSearch.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CTCommon
+{
+    public partial class CTOrderNOSearch : Form
+    {
+        public string ReturnValue;       //元のフォームに返す戻り値
+        public string strOrderMSNo;       //元のフォーム受け取る値（受注先NO）
+        public string strDateTimeFrom;       //元のフォーム受け取る値（受注先NO）
+        public string strDateTimeTo;       //元のフォーム受け取る値（受注先NO）
+
+
+
+        public CTOrderNOSearch()
+        {
+            InitializeComponent();
+        }
+
+        ///////////////////////////////////////////////////
+        //Load処理                                      //
+        //////////////////////////////////////////////////
+        private void CTOrderNOSearch_Load(object sender, EventArgs e)
+        {
+            //クリア処理
+            smClear();
+            //検索処理
+            Search_Main();
+
+        }
+
+
+        ///////////////////////////////////////////////////
+        //ファンクションキー押下処理                    //
+        //////////////////////////////////////////////////
+        private void CTOrderNOSearch_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode){
+                case Keys.F1:
+                    //終了処理
+                    this.Close();
+                    break;
+            }
+        }
+
+        ///////////////////////////////////////////////////
+        //ボタン押下処理                                //
+        //////////////////////////////////////////////////
+        private void button_Click(object sender, EventArgs e)
+        {
+            if (sender.Equals(this.btnEnd)){
+                //終了処理
+                this.Close();
+            }
+        }
+
+        ///////////////////////////////////////////////////
+        //クリア処理                                    //
+        //////////////////////////////////////////////////
+        private void smClear()
+        {
+            //テキストボックスの初期化
+            txtOrderMSNo.Enabled = false;
+            //日付の初期化
+            dtpOrderDateFrom.Enabled = false;
+            dtpOrderDateTo.Enabled = false;
+            //受け取った値の代入
+            txtOrderMSNo.Text = this.strOrderMSNo;
+            dtpOrderDateFrom.Text = this.strDateTimeFrom;
+            dtpOrderDateTo.Text = this.strDateTimeTo;
+
+        }
+
+        ///////////////////////////////////////////////////
+        //検索処理                                      //
+        //////////////////////////////////////////////////
+        private void Search_Main()
+        {
+            //変数定義
+            DataGridViewConnect DataGridViewConnect = new DataGridViewConnect();
+            DataSet dsDataset = new DataSet();
+            string strSQL;
+            //SQL発行
+            strSQL = "";
+            strSQL += "SELECT ";
+            strSQL += " A.受注NO, ";
+            strSQL += " A.受注日, ";
+            strSQL += " A.受注先NO, ";
+            strSQL += " B.受注先名, ";
+            strSQL += " A.受注数, ";
+            strSQL += " A.作業工程NO, ";
+            strSQL += " C.作業工程名 ";
+            strSQL += "FROM ";
+            strSQL += " ORDER_TBL A, ";
+            strSQL += " ORDER_MS B, ";
+            strSQL += " WORKPROCESS_MS C ";
+            strSQL += "WHERE ";
+            strSQL += "    A.受注先NO = '" + txtOrderMSNo.Text + "' ";
+            strSQL += "AND A.受注先NO = B.受注先NO ";
+            strSQL += "AND A.作業工程NO = C.作業工程NO ";
+            strSQL += "AND A.受注チェックフラグ = '0' ";
+            strSQL += "AND A.受注日 BETWEEN ";
+            strSQL += "    '" + dtpOrderDateFrom.Text + "' AND '" + dtpOrderDateTo.Text + "' ";
+            strSQL += "ORDER BY ";
+            strSQL += " A.受注NO ";
+            //SQLを渡し、DataSetを取得する
+            dsDataset = DataGridViewConnect.DataGridViewConnect_Main(strSQL);
+            //DataSetをバインド
+            if (dsDataset != null)
+            {
+                DataGridView1.DataSource = dsDataset.Tables[0];
+            }
+            else
+            {
+                DataGridView1.DataSource = null;
+            }
+
+            //トリム処理
+            for (int i = 0; i <= DataGridView1.RowCount - 1; i++)
+            {
+                for (int y = 0; y <= DataGridView1.ColumnCount - 1; y++) {
+                    DataGridView1[y, i].Value = DataGridView1[y, i].Value.ToString().Trim();
+
+                }
+            }
+
+            //後処理
+            //クローズ処理
+            DBConnect.DBConnect_Close(DBConnect.cn);
+            
+        }
+
+
+        //////////////////////////////////////////////////
+        //DataGridView ダブルクリック処理               //
+        //////////////////////////////////////////////////
+        private void DataGridView1_CellContentDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            //現在行の取得
+            int intRow = e.RowIndex;
+            //セル値の取得
+            string strOrderNo = DataGridView1[0, intRow].Value.ToString();
+            //元フォームに値を返す
+            this.ReturnValue = strOrderNo;
+            this.Close();
+
+        }
+
+
+        //////////////////////////////////////////////////
+        //値返却用フォーム                              //
+        //////////////////////////////////////////////////
+        public string ShowminiForm(string strOrderMSNo, string strDateTimeFrom, string strDateTimeTo)
+        {
+            CTOrderNOSearch CTOrderNOSearch = new CTOrderNOSearch();
+            //受け取った値の代入
+            CTOrderNOSearch.strOrderMSNo = strOrderMSNo;
+            CTOrderNOSearch.strDateTimeFrom = strDateTimeFrom;
+            CTOrderNOSearch.strDateTimeTo = strDateTimeTo;
+            CTOrderNOSearch.ShowDialog();
+            //値を返却
+            string strReciveText = CTOrderNOSearch.ReturnValue;
+            CTOrderNOSearch.Dispose();
+            return strReciveText;
+
+
+
+
+        }
+
+
+
+
+
+
+
+
+    }
+}
