785d414a53b07de235bbc5ff9e1919ab7f06c7e3
SmilePeco <63031036+SmilePeco@users.noreply.github.com>
2020-06-12 11:07:45 +0900

    2020/06/12 追加１

---
+++ Project_c#/CTCommon/CTCommon/CTProcessMSSearch.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace CTCommon
+{
+    partial class CTProcessMSSearch
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.GroupBox2 = new System.Windows.Forms.GroupBox();
+            this.DataGridView1 = new System.Windows.Forms.DataGridView();
+            this.GroupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtProcessMSNo = new System.Windows.Forms.TextBox();
+            this.Label1 = new System.Windows.Forms.Label();
+            this.GroupBox3 = new System.Windows.Forms.GroupBox();
+            this.btnEnd = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.GroupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.DataGridView1)).BeginInit();
+            this.GroupBox1.SuspendLayout();
+            this.GroupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // GroupBox2
+            // 
+            this.GroupBox2.Controls.Add(this.DataGridView1);
+            this.GroupBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.GroupBox2.Location = new System.Drawing.Point(0, 57);
+            this.GroupBox2.Name = "GroupBox2";
+            this.GroupBox2.Size = new System.Drawing.Size(429, 252);
+            this.GroupBox2.TabIndex = 15;
+            this.GroupBox2.TabStop = false;
+            // 
+            // DataGridView1
+            // 
+            this.DataGridView1.AllowUserToAddRows = false;
+            this.DataGridView1.AllowUserToDeleteRows = false;
+            this.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.DataGridView1.Location = new System.Drawing.Point(3, 15);
+            this.DataGridView1.Name = "DataGridView1";
+            this.DataGridView1.ReadOnly = true;
+            this.DataGridView1.RowTemplate.Height = 21;
+            this.DataGridView1.Size = new System.Drawing.Size(423, 234);
+            this.DataGridView1.TabIndex = 0;
+            this.DataGridView1.CellContentDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DataGridView1_CellContentDoubleClick);
+            // 
+            // GroupBox1
+            // 
+            this.GroupBox1.Controls.Add(this.txtProcessMSNo);
+            this.GroupBox1.Controls.Add(this.Label1);
+            this.GroupBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.GroupBox1.Location = new System.Drawing.Point(0, 0);
+            this.GroupBox1.Name = "GroupBox1";
+            this.GroupBox1.Size = new System.Drawing.Size(429, 57);
+            this.GroupBox1.TabIndex = 13;
+            this.GroupBox1.TabStop = false;
+            // 
+            // txtProcessMSNo
+            // 
+            this.txtProcessMSNo.ImeMode = System.Windows.Forms.ImeMode.Disable;
+            this.txtProcessMSNo.Location = new System.Drawing.Point(87, 21);
+            this.txtProcessMSNo.MaxLength = 3;
+            this.txtProcessMSNo.Name = "txtProcessMSNo";
+            this.txtProcessMSNo.Size = new System.Drawing.Size(70, 19);
+            this.txtProcessMSNo.TabIndex = 5;
+            this.txtProcessMSNo.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Text_KeyPress);
+            // 
+            // Label1
+            // 
+            this.Label1.AutoSize = true;
+            this.Label1.Location = new System.Drawing.Point(12, 24);
+            this.Label1.Name = "Label1";
+            this.Label1.Size = new System.Drawing.Size(45, 12);
+            this.Label1.TabIndex = 4;
+            this.Label1.Text = "工程NO";
+            // 
+            // GroupBox3
+            // 
+            this.GroupBox3.Controls.Add(this.btnEnd);
+            this.GroupBox3.Controls.Add(this.btnSearch);
+            this.GroupBox3.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.GroupBox3.Location = new System.Drawing.Point(0, 309);
+            this.GroupBox3.Name = "GroupBox3";
+            this.GroupBox3.Size = new System.Drawing.Size(429, 49);
+            this.GroupBox3.TabIndex = 14;
+            this.GroupBox3.TabStop = false;
+            // 
+            // btnEnd
+            // 
+            this.btnEnd.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnEnd.Location = new System.Drawing.Point(327, 17);
+            this.btnEnd.Name = "btnEnd";
+            this.btnEnd.Size = new System.Drawing.Size(75, 23);
+            this.btnEnd.TabIndex = 3;
+            this.btnEnd.Text = "F2:終了";
+            this.btnEnd.UseVisualStyleBackColor = true;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(6, 17);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 2;
+            this.btnSearch.Text = "F1:検索";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // CTProcessMSSearch
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(429, 358);
+            this.Controls.Add(this.GroupBox2);
+            this.Controls.Add(this.GroupBox1);
+            this.Controls.Add(this.GroupBox3);
+            this.KeyPreview = true;
+            this.Name = "CTProcessMSSearch";
+            this.Text = "工程マスタ検索";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.CTProcessMSSearch_KeyDown);
+            this.GroupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.DataGridView1)).EndInit();
+            this.GroupBox1.ResumeLayout(false);
+            this.GroupBox1.PerformLayout();
+            this.GroupBox3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        internal System.Windows.Forms.GroupBox GroupBox2;
+        public System.Windows.Forms.DataGridView DataGridView1;
+        internal System.Windows.Forms.GroupBox GroupBox1;
+        internal System.Windows.Forms.TextBox txtProcessMSNo;
+        internal System.Windows.Forms.Label Label1;
+        internal System.Windows.Forms.GroupBox GroupBox3;
+        internal System.Windows.Forms.Button btnEnd;
+        internal System.Windows.Forms.Button btnSearch;
+    }
+}
---
+++ Project_c#/CTCommon/CTCommon/CTProcessMSSearch.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace CTCommon
+{
+    public partial class CTProcessMSSearch : Form
+    {
+
+        public string strSQL;
+        public string strReturnValue;
+
+        public CTProcessMSSearch()
+        {
+            InitializeComponent();
+        }
+
+
+        //////////////////////////////////////////////////
+        //ファンクションキー処理                        //
+        //////////////////////////////////////////////////
+        private void CTProcessMSSearch_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode){
+                case Keys.F1:
+                    //検索処理
+                    Search_Main();
+                    break;
+                
+                case Keys.F2:
+                    //終了処理
+                    this.Close();
+                    break;
+
+            }
+
+        }
+
+        //////////////////////////////////////////////////
+        //テキストボックス 数値のみ入力可能             //
+        //////////////////////////////////////////////////
+        private void Text_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //0～9と、バックスペース以外の時は、イベントをキャンセルする
+            if ((e.KeyChar < '0' || '9' < e.KeyChar) && e.KeyChar != '\b')
+            {
+                e.Handled = true;
+            }
+        }
+
+
+        //////////////////////////////////////////////////
+        //ボタン押下処理                               //
+        //////////////////////////////////////////////////
+        private void Button_Click(object sender, EventArgs e)
+        {
+            if (sender.Equals(this.btnSearch)) { Search_Main(); } //検索処理
+            if (sender.Equals(this.btnEnd)) { this.Close(); } //終了処理
+
+        }
+
+        //////////////////////////////////////////////////
+        //検索メイン処理                                //
+        //////////////////////////////////////////////////
+        private void Search_Main()
+        {
+            //０埋め処理
+            if (txtProcessMSNo.Text.Trim() != "") { txtProcessMSNo.Text = txtProcessMSNo.Text.PadLeft(3, '0'); }
+           
+
+            //変数定義
+            DataSet dsDataset = new DataSet();
+            DataGridViewConnect DataGridViewConnect = new DataGridViewConnect();
+            //SQL発行
+            strSQL = "";
+            strSQL += "SELECT ";
+            strSQL += " 工程NO, ";
+            strSQL += " 工程名 ";
+            strSQL += "FROM ";
+            strSQL += " PROCESS_MS ";
+            //空欄の場合は全件検索
+            //空欄以外は入力値を検索条件とする
+            if (txtProcessMSNo.Text.Trim() != ""){
+                strSQL += "WHERE ";
+                strSQL += " 工程NO = '" + txtProcessMSNo.Text.Trim() + "' ";
+
+            }
+            //DataSet取得
+            dsDataset = DataGridViewConnect.DataGridViewConnect_Main(strSQL);
+            //DataSetバインド
+            if (dsDataset != null){
+                DataGridView1.DataSource = dsDataset.Tables[0];
+
+            }else{
+                DataGridView1.DataSource = null;
+
+            }
+
+            //トリム処理
+            for(int i = 0; i <= DataGridView1.RowCount -1; i++){
+                for (int y = 0; y <= DataGridView1.ColumnCount - 1; y++){
+                    DataGridView1[y, i].Value = DataGridView1[y, i].Value.ToString().Trim();
+                }
+
+            }
+
+            //ヘッダーとすべてのセルの内容に合わせて、列の幅を自動調整する
+            DataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+
+            //クローズ処理
+            CTCommon.DBConnect.DBConnect_Close(CTCommon.DBConnect.cn);
+
+
+
+        }
+
+        //////////////////////////////////////////////////
+        //DataGridView ダブルクリック処理               //
+        //////////////////////////////////////////////////
+        private void DataGridView1_CellContentDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            //現在行番号を取得
+            int intRow = e.RowIndex;
+            //行の値を取得
+            string strProcessNo = DataGridView1[0, intRow].Value.ToString();
+            //行の値をかえす
+            strReturnValue = strProcessNo;
+            this.Close();
+
+        }
+
+        //////////////////////////////////////////////////
+        //値返却用フォーム                              //
+        //////////////////////////////////////////////////
+        public string Showminiform()
+        {
+            //変数定義
+            CTProcessMSSearch frmSearch = new CTProcessMSSearch();
+            frmSearch.ShowDialog();
+            string strReturnValue2 = strReturnValue;
+            return strReturnValue2;
+
+        }
+
+
+
+
+
+
+
+
+
+
+    }
+}
