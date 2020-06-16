7c878ea28f5355a9f5c56b1ebfe5337b73d79b2d
SmilePeco <63031036+SmilePeco@users.noreply.github.com>
2020-06-16 19:52:13 +0900

    2020/06/16 最新版

---
+++ Project_c#/CTCommon/CTCommon/ValueCheck.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace CTCommon
+{
+    public class ValueCheck
+    {
+        //変数定義
+        public string strSQL;
+
+        //////////////////////////////////////////////////
+        //更新担当者存在チェック                              //
+        //////////////////////////////////////////////////
+        public Boolean Check_HumanMS(string strHumanNo)
+        {
+            //変数定義
+            SqlCommand cd = null;
+            SqlDataReader dtReader;
+            //SQL発行
+            strSQL = "";
+            strSQL += "SELECT ";
+            strSQL += " 名前 ";
+            strSQL += "FROM ";
+            strSQL += " HUMAN_MS ";
+            strSQL += "WHERE ";
+            strSQL += " 名前 = '" + strHumanNo + "' ";
+            //SQL実行
+            cd = new SqlCommand(strSQL, CTCommon.DBConnect.cn);
+            CTCommon.DBConnect.cn.Open();
+            dtReader = cd.ExecuteReader();
+            if (dtReader.HasRows){
+                //存在しない場合は、FALSEをかえす
+                dtReader.Close();
+                CTCommon.DBConnect.DBConnect_Close(CTCommon.DBConnect.cn);
+                return true;
+
+            }else{
+                //存在しない場合は、FALSEをかえす
+                dtReader.Close();
+                CTCommon.DBConnect.DBConnect_Close(CTCommon.DBConnect.cn);
+                return false;
+
+            }
+
+        }
+
+    }
+}
